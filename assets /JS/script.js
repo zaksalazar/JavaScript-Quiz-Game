ac9de4fbefd04aacee82701cf5e28e3276cc2302
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-10-21 14:15:59 -0700

    started JS

--- assets /JS/script.js
+++ assets /JS/script.js
@@ -1,6 +1,71 @@
-// TODO add variables 
+// TODO add variables, use utility function based on tag 
+const qs = function (tag){
+    return document.getElementById(tag) 
+}
+const startButton = qs('#start-btn');
+const nextButton = qs('#next-btn');
+const questionContainerElment =qs ('#questions-container')
+const answerButton = qs('.answer-btn');
+const timer = qs('.timer');
+const questionElement = qs ('#question')
+const answerButtonsElement = qs('#answer-buttons')
 
-
-//TODO: add event listener for start button "click"
+let shuffledQuestions, currentQuestionIndex
+//TODO: add event listener for start button. When "click" start application 
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 //start timer 
 //display first question 
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+  }
+
+//TODO: create timer component, set interval function in web API. 
+
+//TODO: Store High Scroes in local storage 
+
+// set event listener for next button and generate sequential questions with 3 wrong answers and 1 correct answer. 
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
+
+function selectAnswer(){
+
+}
+//when timer runs out stop timer, display UI, and end game and store score. 
+//When user completes all questions stop timer, display UI, and end game. 
+
+//TODO: Create Questions 
+const questions = [
+    {
+     questions: 'Inside which HTML element do we put the JavaScript?', 
+     answers: [ 
+        { text: "<script>", correct:true },
+        { text: "<js>", correct:false },
+        { text: "<javascript>", correct:false },
+        { text: "<scripting>", correct:false },
+     ]
+    }
+]
