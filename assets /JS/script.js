30c2782d4cd0980ab77fac0f508c916d00a11127
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-10-21 10:33:31 -0700

    added CSS

--- assets /JS/script.js
+++ assets /JS/script.js
@@ -1,43 +1,6 @@
-//variables 
-var score = 0; 
-var questions = [
-    {
-        prompt: "Insert first question here?",
+// TODO add variables 
 
-// answers: 
-     a: "answer one", 
-     b: "answer two",
-     c: "answer three",
-         answer: 
-},
-{
-    prompt: "Insert second question here?",
 
-      a: "answer one",
-      b: "answer two",
-      c: "answer three",
-    answer:
-    },
-
-  {
-    prompt: "Insert third question here?",
-    answers: {
-      a: "answer one",
-      b: "answer two",
-      c: "answer three",  
-      answer: 
-  },
-];
-
-//for loop to loop through questions 
-
-for (var i=o; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt)
-    if (response == questions[i].answer){
-        score ++;
-        alert("Correct!");
-    } else { 
-        alert("Wrong"); 
-}
-}
-alert("you got " + score + "/" + questions.length);
+//TODO: add event listener for start button "click"
+//start timer 
+//display first question 
